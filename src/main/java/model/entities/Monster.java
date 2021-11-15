877c5f488f2a3f1727f69c16bd973d1944225210
Diego Imbert <diegoimbert@pm.me>
2021-11-15 21:33:24 +0100

    fix monster not killed

--- src/main/java/model/entities/Monster.java
+++ src/main/java/model/entities/Monster.java
@@ -13,7 +13,7 @@ import view.DirectedSprite;
 import view.Drawable;
 
 public abstract class Monster extends LivingEntity {
-  
+
   Sound defeated = new Sound();
 
   public Monster(Position position, Size size, DirectedSprite sprite) {
@@ -27,36 +27,30 @@ public abstract class Monster extends LivingEntity {
     Random damage = new Random();
     entity.setInCombat(true);
 
-    new java.util.Timer().schedule( 
-        new java.util.TimerTask() {
-            @Override
-            public void run() {
-              hit.setFile(5);
-              hit.play();
-              setHealth(getHealth() - damage.nextInt(30));
-            }
-        }, 
-        100
-    );
-    
-    new java.util.Timer().schedule( 
-        new java.util.TimerTask() {
-            @Override
-            public void run() {
-              hit.setFile(5);
-              hit.play();
-              entity.setHealth(entity.getHealth() - damage.nextInt(10));
-            }
-        }, 
-        300
-    );
+    new java.util.Timer().schedule(new java.util.TimerTask() {
+      @Override
+      public void run() {
+        hit.setFile(5);
+        hit.play();
+        setHealth(getHealth() - damage.nextInt(30));
 
-    if(this.getHealth() <= 0) {
-      defeated.play();
-      this.setPosition(new Position(0,0,new Room(1000)));
-      entity.setInCombat(false);
-    }
-    
+        if (getHealth() <= 0) {
+          defeated.play();
+          setPosition(new Position(0, 0, new Room(1000)));
+          entity.setInCombat(false);
+        }
+      }
+    }, 100);
+
+    new java.util.Timer().schedule(new java.util.TimerTask() {
+      @Override
+      public void run() {
+        hit.setFile(5);
+        hit.play();
+        entity.setHealth(entity.getHealth() - damage.nextInt(10));
+      }
+    }, 300);
+
   }
 
   @Override
