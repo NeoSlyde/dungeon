4e9667977a0fe3aa67068428f37b9a1906d09e9d
Diego Imbert <diegoimbert@pm.me>
2021-11-23 00:25:41 +0100

    small refactor

--- src/main/java/model/Scheduler.java
+++ src/main/java/model/Scheduler.java
@@ -3,6 +3,8 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.misc.Updatable;
+
 // A scheduler that can be updated.
 // This prevents asynchronous events from happening between updates
 public class Scheduler implements Updatable {
