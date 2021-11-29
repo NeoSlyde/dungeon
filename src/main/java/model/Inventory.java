2f2099e95dccc09a55e4fa6e334497766640437f
Bassem MSAYIF <bassem.msayif@etu.univ-amu.fr>
2021-11-29 22:52:19 +0100

    Inventory Upgrades

--- src/main/java/model/Inventory.java
+++ src/main/java/model/Inventory.java
@@ -3,20 +3,40 @@ package model;
 import java.util.HashMap;
 import java.util.Map;
 
+import model.items.Item;
+
 public class Inventory {
 
     private Map<Item, Integer> items = new HashMap<>();
+    private int capacity = 12;
 
     public void addItem(Item item, int amount) {
-        if (items.containsKey(item)) {
-            items.put(item, items.get(item) + amount);
-        } else {
-            items.put(item, amount);
+        // ensure inventory has capacity
+        if (items.size() < capacity) {
+            // if item is already in inventory, add to amount
+            if (items.containsKey(item)) {
+                items.put(item, items.get(item) + amount);
+            } else {
+                // if item is not in inventory, add item and amount
+                items.put(item, amount);
+            }
         }
     }
 
+    public Map<Item, Integer> getItems() {
+        return items;
+    }
+
     public void removeItem(Item item, int amount) {
-        items.computeIfPresent(item, (k, v) -> v - amount > 0 ? v - amount : null);
+        items.computeIfPresent(item, (k, v) -> v - amount > 0 ? v - amount : 0);
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    public int getCapacity() {
+        return capacity;
     }
 
 }
--- src/main/java/model/Item.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-import model.entities.LivingEntity;
-
-public abstract class Item {
-    public abstract void use(LivingEntity user);
-}
