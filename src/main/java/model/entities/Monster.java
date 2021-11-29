1a948b47aedc0c485c9961b1fe04e56fda45cd44
Bassem MSAYIF <bassem.msayif@etu.univ-amu.fr>
2021-11-29 20:35:34 +0100

    Skeleton Mob

---
+++ src/main/java/model/entities/Monster.java
@@ -0,0 +1,19 @@
+package model.entities;
+
+import model.Room;
+import model.misc.Vec2;
+import view.DrawableVisitor;
+
+public abstract class Monster extends LivingEntity {
+    protected double health = getMaxHealth();
+
+    public Monster(Room room, Vec2 position) {
+        super(room, position, new Vec2(0.5, 0.5));
+    }
+
+    @Override
+    public void draw(DrawableVisitor d) {
+        d.draw(this);
+    }
+
+}
---
+++ src/main/java/model/entities/Skeleton.java
@@ -0,0 +1,38 @@
+package model.entities;
+
+import model.Room;
+import model.misc.Vec2;
+import view.DrawableVisitor;
+
+public class Skeleton extends Monster {
+
+    public Skeleton(Room room, Vec2 position) {
+        super(room, position);
+    }
+
+    @Override
+    public double getMaxHealth() {
+        return 50;
+    }
+
+    @Override
+    public double getHealth() {
+        return health;
+    }
+
+    @Override
+    public double getStrength() {
+        return 5;
+    }
+
+    @Override
+    public double getSpeed() {
+        return 4;
+    }
+
+    @Override
+    public void draw(DrawableVisitor d) {
+        d.draw(this);
+    }
+
+}
